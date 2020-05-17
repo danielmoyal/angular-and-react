b603c53128388bce9caef5224c667c7a2a2fd69e
daniel <danielmoyal2016@gmail.com>
2020-05-17 15:01:53 +0300

    fix(webpack) : fix webpack config & update package.json versions to corresponds our project

--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,55 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    template: './app/index.html',
-    filename: './index.html'
+  template: 'app/index.html',
+  filename: 'index.html'
 });
 
 module.exports = {
-    mode: process.env.NODE_ENV,
-    entry: {
-        app: './app/app.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true,
-        compress: true,
-        port: 9000
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        htmlPlugin
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                exclude: /node_modules/,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
+  mode: process.env.NODE_ENV,
+  entry: {
+    app: './app/app.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    compress: true,
+    port: 9000
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    htmlPlugin
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
 
-                    // Translates CSS into CommonJS
-                    'css-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
 
-                    // Compiles Sass to CSS
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader',
-                ],
-            }
+          // Compiles Sass to CSS
+          'sass-loader',
+          "html-loader"
         ]
-    }
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          'file-loader',
+        ]
+      },
+    ]
+  }
 };
